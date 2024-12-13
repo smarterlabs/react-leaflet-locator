2cabe62db58d5c4d821c6b09a52ffaaa8c5ab678
Kennedy Rose <kennedyianrose@gmail.com>
2024-12-13 12:44:10 -0600

    Removed search limit

--- src/LeafletMap/Search.js
+++ src/LeafletMap/Search.js
@@ -32,7 +32,7 @@ export default function Search(props){
 
 	const osmProvider = OpenStreetMapProvider && new OpenStreetMapProvider({
 		params: {
-			countrycodes: [`ca`,`us`], // limit search results to the Netherlands
+			// countrycodes: [`ca`,`us`],
 			addressdetails: 1, 
 			format: `json`,
 		},
